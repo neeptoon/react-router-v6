6f590127490401fb91740f5483372c88d82cabce
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-06-19 20:33:03 +0300

    add basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import {
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+import {About} from './pages/Aboutpage';
+import {Homepage} from "./pages/Homepage";
+import {Blogpage} from "./pages/Blogpage";
+import {Notfoundpage} from "./pages/Notfoundpage";
+
 function App() {
   return (
     <>
       <header>
-        <a href="/">Home</a>
-        <a href="/posts">Blog</a>
-        <a href="/about">About</a>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Blog</Link>
+        <Link to="/about">About</Link>
       </header>
-      <div>
-            <h1>Get started with React-Router 6</h1>
-        </div>
+        <Routes>
+            <Route path='/' element={<Homepage/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/posts' element={<Blogpage/>}/>
+            <Route path='*' element={<Notfoundpage/>}/>
+        </Routes>
     </>
   );
 }
